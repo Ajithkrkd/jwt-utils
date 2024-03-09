aa4dcd62b5f97f32cb0da202f67f2c3c6d859507
Ajith krkd <ajith2255iti@gmail.com>
2024-03-09 12:39:25 +0530

    changed the extract username method

--- src/main/java/com/ajith/jwtutilpackage/jwt/JwtService.java
+++ src/main/java/com/ajith/jwtutilpackage/jwt/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.StringUtils;
 
@@ -13,6 +14,8 @@ import java.security.Key;
 import java.util.Date;
 import java.util.List;
 import java.util.function.Function;
+
+@Slf4j
 
 
 public class JwtService {
@@ -24,16 +27,19 @@ public class JwtService {
     @Value ( "${application.security.jwt.refresh-token.expiration}" )
     private static long refreshExpiration ;
 
-    public static String extractUsername (String token) {
-        return extractClaim (token, Claims::getSubject);
-    }
-
 
     public static <T>T extractClaim (String token, Function < Claims, T > claimsResolver) {
         final Claims claims = extractAllClaims ( token );
         return claimsResolver.apply ( claims );
     }
-
+    public static String getUsernameFromToken(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey ())
+                .build ()
+                .parseClaimsJws(token)
+                .getBody();
+        return  claims.getSubject();
+    }
 
     public List <String> extractRolesFromToken(String token) {
         Claims claims = Jwts.parserBuilder ()
